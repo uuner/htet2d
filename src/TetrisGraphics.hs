108cb2dbf472107e682fed2b49990015cacc8cd2
Julia Jomantaite <julia.jomantaite@gmail.com>
2012-05-20 18:35:14 -0400

    wiping PAUSED properly

--- src/TetrisGraphics.hs
+++ src/TetrisGraphics.hs
@@ -23,8 +23,8 @@ fieldStr gs = map (map fun) z
    fArr = (replicate (max (y - 1) 0) (replicate fieldWidth 0)) ++
      map (\a -> (replicate (x - 1) 0) ++ 
                  (drop (1-x) a) ++ 
-		 (replicate (max (fieldWidth - fromIntegral x - w + 1) 0) 0)) 
-	 (drop (1-y) $ shape fig) ++
+         (replicate (max (fieldWidth - fromIntegral x - w + 1) 0) 0)) 
+     (drop (1-y) $ shape fig) ++
      replicate (max (fieldHeight - y - h + 1) 0) (replicate fieldWidth 0)
    w = length $ head $ shape fig
    h = length $ shape fig
@@ -40,13 +40,13 @@ visualize gs oldgs = do
   where 
   outstr = "\27[2J\27[H" ++ 
            concatMap (++"\n") (border strs) ++ 
-	   moveCursor 16 3 ++ "Next:" ++ 
-	   drawNext 16 4 ++ 
-	   moveCursor 16 9 ++ "Score:" ++
-	   drawScores 17 10 ++
-	   moveCursor 16 12 ++ "Level:" ++
-	   drawLevel 18 13 ++
-	   (moveCursor 1 (3+fieldHeight))
+       moveCursor 16 3 ++ "Next:" ++ 
+       drawNext 16 4 ++ 
+       moveCursor 16 9 ++ "Score:" ++
+       drawScores 17 10 ++
+       moveCursor 16 12 ++ "Level:" ++
+       drawLevel 18 13 ++
+       (moveCursor 1 (3+fieldHeight))
   strs = colorize gs
   horiz st = "+"++(replicate fieldWidth '-')++"+"
   border st = (horiz st): (map (\a -> "|"++ a ++ "|") st ++ [horiz st])
@@ -55,23 +55,23 @@ visualize gs oldgs = do
   empt n = replicate n []
   changestr = concat (zipWith (\(a,b,c) (a', b', c') -> 
                    if c /= c' 
-       		   then ((moveCursor (1+a) (1+b))++coloredCell c)
-       		   else "")
+                  then ((moveCursor (1+a) (1+b))++coloredCell c)
+                  else "")
                       (g $ fieldStr gs) (g $ fieldStr oldgs))
               ++ (if getScores gs /= getScores oldgs then drawScores 17 10 else "")
               ++ (if getLevel gs /= getLevel oldgs then drawLevel 18 13 else "")
               ++ (if getNext gs /= getNext oldgs then drawNext 16 4 else "")
-	      ++ (if isOver gs then moveCursor 2 10 ++ "GAME OVER!" else "")
-	      ++ drawPause
+          ++ (if isOver gs then moveCursor 2 10 ++ "GAME OVER!" else "")
+          ++ drawPause
               ++ (moveCursor 1 (3+fieldHeight))
   moveCursor x y = "\27["++show y++';':(show x)++"f"
   drawNext x y = concat [moveCursor x (y+n) ++ nextFig!!n | n <- [0..(length nextFig - 1)]]
   nextFig = map (concatMap (\a -> if a == 0 then " " else coloredCell (color (Just nf), "X"))) $
-       	   upTo4 [0,0,0,0] (map (upTo4 0) 
-       	        (shape next))
+              upTo4 [0,0,0,0] (map (upTo4 0) 
+                   (shape next))
   drawPause
     | isPaused gs && not (isPaused oldgs) = moveCursor 16 11 ++ "PAUSED"
-    | isPaused oldgs && not (isPaused gs) = moveCursor 16 11 ++ "     "
+    | isPaused oldgs && not (isPaused gs) = moveCursor 16 11 ++ "      "
     | True = ""
     
   drawScores x y = moveCursor x y ++ (show $ getScores gs)
@@ -79,7 +79,7 @@ visualize gs oldgs = do
   g a = concat $ map (\m -> [(n+1,fst (m!!n), snd (m!!n)) | 
                               n <-[0..length m - 1]])
                      [map (\m -> (n+1,m))(a!!n) | 
-       	           n <-[0..length a - 1]]
+                      n <-[0..length a - 1]]
 
 color x = case x of
   Just FigI -> 36
