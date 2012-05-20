8071144fdb1ffb079392afa9facb244be60fdf53
Julia Jomantaite <julia.jomantaite@gmail.com>
2012-05-20 19:00:46 -0400

    more whitespace/style changes

--- src/Tetris.hs
+++ src/Tetris.hs
@@ -1,3 +1,4 @@
+import Control.Monad (void)
 import Data.Array
 import System.IO
 import System.Random
@@ -12,55 +13,47 @@ main = do oldEcho <- hGetEcho stdin
           hSetBuffering stdout NoBuffering
           rand1 <- pickRandom
           rand2 <- pickRandom
-          time <- getClockTime
-          let empt = emptyField rand1 rand2
-            in processio (startGame empt) empt time
+          let newField = emptyField rand1 rand2
+          processio (startGame newField) newField =<< getClockTime
           hSetEcho stdin oldEcho
 
 getMSeconds :: ClockTime -> Integer
-getMSeconds (TOD sec psec) = sec*1000 + div psec (10^9) 
+getMSeconds (TOD sec psec) = sec * 1000 + div psec (10 ^ 9)
 
 toWait :: ClockTime -> ClockTime -> Integer -> Int
 toWait new old mseclvl = fromIntegral (max 0 (mseclvl - (getMSeconds new - getMSeconds old)))
 
 pickRandom :: IO Int
-pickRandom = getStdRandom (randomR (0,4*figNum-1))
+pickRandom = getStdRandom (randomR (0, 4 * figNum - 1))
 
 processio :: GameStatus -> GameStatus -> ClockTime -> IO ()
 processio gs oldgs lastdown
-  | isOver gs = visualize gs oldgs >> return()
+  | isOver gs = void $ visualize gs oldgs
   | isPaused gs = do visualize gs oldgs
                      key <- getKey
-                     if key == "p"
-                     then getClockTime >>= processio (startGame gs) gs
-                     else
-                       if key == "q" 
-                       then 
-                         return () 
-                       else 
-                         processio gs oldgs lastdown
+                     case key of
+                        "p" -> processio (startGame gs) gs =<< getClockTime
+                        "q" -> return ()
+                        _   -> processio gs oldgs lastdown
   | otherwise = do visualize gs oldgs
                    time <- getClockTime
                    hasInput <- hWaitForInput stdin $! toWait time lastdown (lvlDelay (getLevel gs))
                    if not hasInput
                    then do rand <- pickRandom
-                           downtime <- getClockTime
-                           processio (timeStep gs rand) gs downtime
+                           processio (timeStep gs rand) gs =<< getClockTime
                    else do key <- getKey
-                           if key /= "q"
-                           then
-                             processio (keyAction gs key) gs lastdown
-                           else
-                             return ()
+                           case key of
+                              "q" -> return ()
+                              _   -> processio (keyAction gs key) gs lastdown
 
 getKey = do key <- getChar
-            if key == '\27' 
+            if key == '\27'
             then do key1 <- getChar
                     if key1 == '['
                     then do key2 <- getChar
-                            return [key,key1,key2]
+                            return [key, key1, key2]
                     else
-                        return [key,key1]
+                        return [key, key1]
             else
                 return [key]
 
@@ -69,7 +62,7 @@ timeStep gs rnum
   | can $ moveDown gs = moveDown gs
   | otherwise = concrete gs rnum
 
-keyAction :: GameStatus -> [Char] -> GameStatus
+keyAction :: GameStatus -> String -> GameStatus
 keyAction gs key
   | key == "p" = pauseGame gs
   | key == "a" || key == "\27[D" = tryDo (moveLeft gs) gs
@@ -80,5 +73,4 @@ keyAction gs key
   | otherwise = gs
   where tryDo moved original = if can moved then moved else original
 
-lvlDelay n = ([1000, 900..200] ++ [150] ++ repeat 100) !! n
-
+lvlDelay n = ([1000, 900 .. 200] ++ [150] ++ repeat 100) !! n
